0135846b9d14e5a1baf322cf372786a2bd2f9a6e
daniel <danielrodriguescarvalho777@gmail.com>
2024-03-04 16:16:06 -0300

    adjusting the buttons to change themes

---
+++ src/toggle-theme.js
@@ -0,0 +1,69 @@
+let defaultTheme = true;
+const btnForest = document.getElementById("forest");
+const btnRain = document.getElementById("rain");
+const btnBookstore = document.getElementById("bookstore");
+const btnFireplace = document.getElementById("fireplace");
+
+btnForest.addEventListener("click", (event) => {
+  const element = document.querySelector("html");
+  const classeAtual = element.getAttribute("class");
+  element.setAttribute("class", classeAtual.replace(classeAtual, "forest"));
+
+  const img = document.querySelector("#forest");
+  img.src = "./assets/img/forest-selected.svg";
+
+  const imgr = document.querySelector("#rain");
+  imgr.src = "./assets/img/rain.svg";
+  const imgb = document.querySelector("#bookstore");
+  imgb.src = "./assets/img/bookstore.svg";
+  const imgfp = document.querySelector("#fireplace");
+  imgfp.src = "./assets/img/fireplace.svg";
+});
+
+btnRain.addEventListener("click", (event) => {
+  const element = document.querySelector("html");
+  const classeAtual = element.getAttribute("class");
+  element.setAttribute("class", classeAtual.replace(classeAtual, "rain"));
+
+  const img = document.querySelector("#rain");
+  img.src = "./assets/img/rain-selected.svg";
+
+  const imgf = document.querySelector("#forest");
+  imgf.src = "./assets/img/forest.svg";
+  const imgb = document.querySelector("#bookstore");
+  imgb.src = "./assets/img/bookstore.svg";
+  const imgfp = document.querySelector("#fireplace");
+  imgfp.src = "./assets/img/fireplace.svg";
+});
+
+btnBookstore.addEventListener("click", (event) => {
+  const element = document.querySelector("html");
+  const classeAtual = element.getAttribute("class");
+  element.setAttribute("class", classeAtual.replace(classeAtual, "bookstore"));
+
+  const img = document.querySelector("#bookstore");
+  img.src = "./assets/img/bookstore-selected.svg";
+
+  const imgf = document.querySelector("#forest");
+  imgf.src = "./assets/img/forest.svg";
+  const imgr = document.querySelector("#rain");
+  imgr.src = "./assets/img/rain.svg";
+  const imgfp = document.querySelector("#fireplace");
+  imgfp.src = "./assets/img/fireplace.svg";
+});
+
+btnFireplace.addEventListener("click", (event) => {
+  const element = document.querySelector("html");
+  const classeAtual = element.getAttribute("class");
+  element.setAttribute("class", classeAtual.replace(classeAtual, "fireplace"));
+
+  const img = document.querySelector("#fireplace");
+  img.src = "./assets/img/fireplace-selected.svg";
+
+  const imgb = document.querySelector("#bookstore");
+  imgb.src = "./assets/img/bookstore.svg";
+  const imgf = document.querySelector("#forest");
+  imgf.src = "./assets/img/forest.svg";
+  const imgr = document.querySelector("#rain");
+  imgr.src = "./assets/img/rain.svg";
+});
