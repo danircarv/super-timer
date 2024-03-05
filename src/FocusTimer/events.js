563efd1f569ad7aadc9733a793f23de3c259007b
daniel <danielrodriguescarvalho777@gmail.com>
2024-03-04 21:50:41 -0300

    configuring data-action

--- src/FocusTimer/events.js
+++ src/FocusTimer/events.js
@@ -1,1 +1,12 @@
-export function registerControls() {}
+import { controls } from "./elements.js";
+
+export function registerControls() {
+  controls.addEventListener("click", (event) => {
+    const action = console.log(event.target.dataset.action);
+    if (action === undefined) {
+      return;
+    }
+
+    console.log(action);
+  });
+}
